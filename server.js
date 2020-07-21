54706ad77d2130a8eb351be39f5e9c57615fcd7f
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-20 23:47:59 -0700

    added real time chat

---
+++ server.js
@@ -0,0 +1,76 @@
+
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+
+
+
+var db = require("./models");
+
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.set("views", "./views");
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+server.listen(3300)
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+
+
+
+
+
+module.exports = app;
