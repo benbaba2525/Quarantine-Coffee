a68c0e4267064bb34721106e64b3349f04baab0b
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-27 00:25:23 -0700

    Css for chat message

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,27 +7,27 @@ const messageInput = document.getElementById('message-input')
 
 const name = prompt('What is your name?')
 document.write(`<div class="text"><h1 style="text-align:center; padding:20px;background-color:rgb(111, 224, 237);margin:15px;height:100%;"> <i class="fa fa-comments-o" aria-hidden="true"></i>  <strong> ${name}</strong> </h1></div>`)
-appendMessage('You joined!!')
+appendMessage(`<h5><strong>You joined!!</strong></h5>`)
 socket.emit('new-user', name)
 
 
 
 socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
+  appendMessage(`<h5><strong> ${data.name} : ${data.message} </strong></h5>`)
 })
 
 socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
+  appendMessage(`<h5><strong> ${name} connected </strong></h5>`)
 })
 
 socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
+  appendMessage(`<h5><strong> ${name} disconnected</strong></h5>`)
 })
 
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
-  appendMessage(`You: ${message}`)
+  appendMessage(`<h5><strong>You: ${message}</strong></h5>`)
   socket.emit('send-chat-message', message)
   messageInput.value = ''
 })
