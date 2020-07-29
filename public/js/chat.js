5e15cb0cbf3c52d1408969c245857709d57755d7
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-28 23:07:03 -0700

    test socket.io client

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
 const socket = io('http://localhost:9090')
 
+
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require("express-handlebars");
 
 
 var app = express();
-var PORT = process.env.PORT || 9000 || 9090;
+var PORT = process.env.PORT || 9000 || 9090 ;
 //--- Real time chat -------
 const server = require("http").Server(app);
 const io = require('socket.io')(server);
