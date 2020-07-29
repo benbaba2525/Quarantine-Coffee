615a05a8ec3ab9b11d0c5876e6550e4795294f04
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-28 19:09:59 -0700

    change localhost to test

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3300')
+const socket = io('http://localhost:9090')
 
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
