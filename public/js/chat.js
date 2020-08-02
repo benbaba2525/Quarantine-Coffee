5dc39351105da02ece087e38abe0987a5746f045
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-01 22:23:25 -0700

    io();

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3300')
+const socket = io();
 
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
