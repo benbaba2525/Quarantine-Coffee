90c4582aa1bb9faabb5ac711d6eab383de5b3299
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-02 08:34:45 -0700

    test connection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require("express-handlebars");
 
 
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = 5000;
 //--- Real time chat -------
 const server = require("http").Server(app);
 const io = require('socket.io')(server);
@@ -31,7 +31,7 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Reat Time Chat
-server.listen(3300)
+server.listen( process.env.PORT || 3300)
 const users = {}
 
 io.on('connection', socket => {
